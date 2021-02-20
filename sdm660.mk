29e2f5025e765f4ead7cb8df83b70765c7098006
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-20 19:50:35 +0530

    sdm660-common: arrow -> havoc

--- sdm660.mk
+++ sdm660.mk
@@ -269,7 +269,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-havoc
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
