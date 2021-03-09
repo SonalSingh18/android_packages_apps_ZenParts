bb2903dec69de1f2fa214fa34b9fc1846cf08631
Sonal Singh <sonal.singh.19993@gmail.com>
2021-03-09 14:11:29 +0000

    sdm660-common: havoc -> dot
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -269,7 +269,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-havoc
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
